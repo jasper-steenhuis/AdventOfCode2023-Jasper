5723c23742bb7a3ac64427c2b52c6fe519d52796
jasper <steenhuisjasper1@gmail.com>
2023-12-01 23:50:03 +0100

    It's really ugly but it works...

---
+++ DayOne.java
@@ -0,0 +1,55 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+class DayOne{
+    public static void main(String[] args) {  
+        try {
+            File inputFile = new File("C:\\Users\\Gebruiker\\Documents\\projecten\\AdventOfCode2023-Jasper\\InputFiles\\dayOne");
+            Scanner fileScanner = new Scanner(inputFile); 
+            ArrayList<String> stringList = new ArrayList<String>();
+        while (fileScanner.hasNextLine()) {
+            String data = fileScanner.nextLine();
+            stringList.add(data);
+        }
+        fileScanner.close();
+        ArrayList<Integer> ints = new ArrayList<Integer>();
+        int total =0;
+
+        for(int i=0; i<stringList.size(); i++){
+            String[] strs;
+            strs = extractNumbersFromStrings(stringList.get(i));
+            for(int x=0; x< strs.length; x++){
+                    int first = strs[x].charAt(0) - '0';
+                    int last = strs[x].charAt(strs[x].length()-1) - '0';
+                    String firstStr = String.valueOf(first);
+                    String lastStr = String.valueOf(last);
+                    String finalStr = firstStr + lastStr;
+                    int toAdd = Integer.parseInt(finalStr);
+                    total += toAdd;
+                    System.out.println(finalStr);
+    
+            }
+            System.out.println("TOTAL: " + total);
+        }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        } 
+        
+    }  
+    public static String[] extractNumbersFromStrings(String str){
+           return str.replaceAll("[^0-9]","").split("\\D+");        
+    }
+
+
+    
+
+
+
+            
+
+
+} 
